b0f851494b21d226628ada17107c73e87545476b
geoffreynyaga <geoffreynyagak@gmail.com>
2020-02-20 15:21:09 +0300

    added validators and slug

--- classroom/models.py
+++ classroom/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.utils.text import slugify
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+
+def validate_negative(value):
+    if value < 0:
+        raise ValidationError(
+            _("%(value)s is not an posituve number"), params={"value": value},
+        )
 
 
 class Student(models.Model):
@@ -6,25 +17,34 @@ class Student(models.Model):
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+
+    username = models.SlugField(blank=True, null=True)
+
     admission_number = models.IntegerField(unique=True)
 
     is_qualified = models.BooleanField(default=False)
 
-    average_score = models.FloatField(blank=True, null=True)
+    average_score = models.FloatField(
+        blank=True, null=True, validators=[validate_negative]
+    )
 
     def __str__(self):
         """Unicode representation of Student."""
         return self.first_name
 
     def get_grade(self):
-        if self.average_score < 40:
+        if 0 <= self.average_score < 40:
             return "Fail"
-        elif 40 < self.average_score < 70:
+        elif 40 <= self.average_score < 70:
             return "Pass"
-        elif 70 < self.average_score < 100:
+        elif 70 <= self.average_score < 100:
             return "Excellent"
         else:
             return "Error"
+
+    def save(self, *args, **kwargs):
+        self.username = slugify(self.first_name)
+        super(Student, self).save(*args, **kwargs)
 
 
 class Classroom(models.Model):
