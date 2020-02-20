d410fa8e081b089e76a2e698956cd941349b35fd
geoffreynyaga <geoffreynyagak@gmail.com>
2020-02-20 15:21:24 +0300

    added hypothesis

--- classroom/tests.py
+++ classroom/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+# from django.test import TestCase
+from hypothesis.extra.django import TestCase
 import pytest
+from hypothesis import strategies as st, given
 from classroom.models import Student, Classroom
 
 from mixer.backend.django import mixer
@@ -41,33 +43,51 @@ class TestStudentModel(TestCase):
 
         assert str(student_result) == "Tom"
 
-    def test_grade_fail(self):
+    @given(st.characters())
+    def test_slugify(self, name):
 
-        student1 = mixer.blend(Student, average_score=10)
+        print(name, "name")
+
+        student1 = mixer.blend(Student, first_name=name)
+        student1.save()
+
+        student_result = Student.objects.last()  # getting the last student
+
+        assert len(str(student_result.username)) == len(name)
+
+    @given(st.floats(min_value=0, max_value=40))
+    def test_grade_fail(self, fail_score):
+
+        print(fail_score, "this is failscore")
+
+        student1 = mixer.blend(Student, average_score=fail_score)
 
         student_result = Student.objects.last()  # getting the last student
 
         assert student_result.get_grade() == "Fail"
 
-    def test_grade_pass(self):
+    @given(st.floats(min_value=40, max_value=70))
+    def test_grade_pass(self, pass_grade):
 
-        student1 = mixer.blend(Student, average_score=60)
+        student1 = mixer.blend(Student, average_score=pass_grade)
 
         student_result = Student.objects.last()  # getting the last student
 
         assert student_result.get_grade() == "Pass"
 
-    def test_grade_excellent(self):
+    @given(st.floats(min_value=70, max_value=100))
+    def test_grade_excellent(self, excellent_grade):
 
-        student1 = mixer.blend(Student, average_score=90)
+        student1 = mixer.blend(Student, average_score=excellent_grade)
 
         student_result = Student.objects.last()  # getting the last student
 
         assert student_result.get_grade() == "Excellent"
 
-    def test_grade_error(self):
+    @given(st.floats(min_value=100))
+    def test_grade_error(self, error_grade):
 
-        student1 = mixer.blend(Student, average_score=101)
+        student1 = mixer.blend(Student, average_score=error_grade)
 
         student_result = Student.objects.last()  # getting the last student
 
