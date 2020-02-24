4446fdddcad2a655359f0f59f39ec59bc7d687a3
geoffreynyaga <geoffreynyagak@gmail.com>
2020-02-24 23:41:38 +0300

    classroom.api.urls

--- School/urls.py
+++ School/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("classroom.api.urls")),
+    re_path(r"^api-auth/", include("rest_framework.urls")),
 ]
