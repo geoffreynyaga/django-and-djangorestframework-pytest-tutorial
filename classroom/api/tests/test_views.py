718d576bc35205dffe1ddc335046ad1f43de4f4a
geoffreynyaga <geoffreynyagak@gmail.com>
2020-02-25 23:08:10 +0300

    test_student_create_works test_student_detail_works test_student_delete_works

--- classroom/api/tests/test_views.py
+++ classroom/api/tests/test_views.py
@@ -39,3 +39,73 @@ class TestStudentAPIViews(TestCase):
 
         assert response.status_code == 200
 
+    def test_student_create_works(self):
+        # data
+
+        input_data = {
+            "first_name": "Wangari",
+            "last_name": "Maathai",
+            "username": "",
+            "admission_number": 9876,
+            "is_qualified": True,
+            "average_score": 100,
+        }
+
+        url = reverse("student_create_api")
+
+        # call the url
+        response = self.client.post(url, data=input_data)
+
+        # assertions
+        # - json
+        # - status
+
+        print(response.data)
+        assert response.json() != None
+        assert response.status_code == 201
+        assert Student.objects.count() == 1
+
+    def test_student_detail_works(self):
+        # create a student
+
+        student = mixer.blend(Student, first_name="Geoffrey")
+        url = reverse("student_detail_api", kwargs={"pk": 1})
+        response = self.client.get(url)
+
+        student2 = mixer.blend(Student, first_name="Naomi")
+        url2 = reverse("student_detail_api", kwargs={"pk": 2})
+        response2 = self.client.get(url2)
+
+        # assertions
+        # - json
+        # - status
+
+        print(response.json(), "response json")
+
+        assert response.json() != None
+        assert response.status_code == 200
+        assert response.json()["first_name"] == "Geoffrey"
+        assert response.json()["username"] == "geoffrey"
+
+        assert response2.json()["first_name"] == "Naomi"
+        assert response2.json()["username"] == "naomi"
+
+    def test_student_delete_works(self):
+        # create a student
+
+        student = mixer.blend(Student, first_name="Geoffrey")
+        assert Student.objects.count() == 1
+
+        url = reverse("student_delete_api", kwargs={"pk": 1})
+        response = self.client.delete(url)
+        # assertions
+        # - json
+        # - status
+
+        print(dir(response.json), "response json")
+        print((response.status_code), "response json")
+
+        assert response.status_code == 204
+
+        assert Student.objects.count() == 0
+
