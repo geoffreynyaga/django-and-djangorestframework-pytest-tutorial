18be7364406036715ca6fff3e910f34ff4c78b03
geoffreynyaga <geoffreynyagak@gmail.com>
2020-03-02 22:48:58 +0300

    api-token-auth/

--- School/urls.py
+++ School/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 
+from rest_framework.authtoken import views
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("classroom.api.urls")),
     re_path(r"^api-auth/", include("rest_framework.urls")),
+    re_path(r"^api-token-auth/", views.obtain_auth_token),
 ]
